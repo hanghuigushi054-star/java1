e78ead89e04e750754259eb9b11fccf070593832
kosei <hanghuigushi054@gmail.com>
2026-05-21 14:05:14 +0900

    Add files via upload

---
+++ day02.java
@@ -0,0 +1,13 @@
+/* こんにちはという文字を表示するプログラム ２０２６/05/21*/
+
+
+public class day02 {
+public static void main(String[] args) {
+    // こんにちはという文字を表示する
+    System.out.println("こんにちは");
+        System.out.println(2+3);
+        int i;
+        i = 5;
+        System.out.println(i);
+    }    
+}
