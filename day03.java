803fe6f1ad0e0630b56992704cb41dc155f8e2ae
kosei <hanghuigushi054@gmail.com>
2026-05-24 12:10:20 +0900

    Add files via upload

---
+++ day03.java
@@ -0,0 +1,28 @@
+public class day03 {
+
+    public static void main(String[] args) {
+    int i;
+    i = 5;
+    System.out.println(i);   
+    double a;
+    a  = 3.14;
+    System.out.println(a);
+    boolean c;
+    c = true;
+    System.out.println(c);
+    System.out.println(3 + 3);
+    System.out.println(5 - 3);
+    System.out.println(3 * 3);
+    System.out.println(3 / 3);
+    System.out.println(7 % 3);
+    int x = 5;
+    int j = x * 2;
+    System.out.println(j);
+    x = x + 3;
+    System.out.println(x);
+    x += 4;
+    System.out.println(x);
+
+    
+    }
+}
